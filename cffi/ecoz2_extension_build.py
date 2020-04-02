c7a64ec145d7657ed3cf42b24210c32eb18f7976
Carlos Rueda <carueda@mbari.org>
2020-04-01 20:55:33 -0700

    cffi: compiling all c sources ok

--- cffi/ecoz2_extension_build.py
+++ cffi/ecoz2_extension_build.py
@@ -9,12 +9,53 @@ typedef void (*callback_t)(char*, int);
 void ecoz2_do_filenames(char* filenames[], int num_filenames, callback_t callback);
 """)
 
-ffibuilder.set_source("_ecoz2_extension",  # name of the output C extension
-"""
-    #include "ecoz2_extension.h"
-""",
-    sources=['ecoz2_extension.c'],   # includes ecoz2_extension.c as additional sources
-    libraries=['m'])                 # on Unix, link with the math library
+ffibuilder.set_source(
+  "_ecoz2_extension",
+  """
+  #include "ecoz2_extension.h"
+  """,
+  sources=[
+    'ecoz2_extension.c',
+    '../ecoz2/src/utl/utl.c',
+    '../ecoz2/src/utl/fileutil.c',
+    '../ecoz2/src/utl/list.c',
+    '../ecoz2/src/utl/memutil.c',
+    '../ecoz2/src/sgn/sgn.c',
+    '../ecoz2/src/sgn/dr_wav.c',
+    '../ecoz2/src/lpc/lpc_signals.c',
+    '../ecoz2/src/lpc/lpaOnSignal.c',
+    '../ecoz2/src/lpc/lpca.c',
+    '../ecoz2/src/lpc/prd.c',
+    '../ecoz2/src/lpc/prd_show_file.c',
+    '../ecoz2/src/lpc/ref2raas.c',
+    '../ecoz2/src/vq/vq_learn.c',
+    '../ecoz2/src/vq/vq_quantize.c',
+    '../ecoz2/src/vq/vq_classify.c',
+    '../ecoz2/src/vq/vq_show.c',
+    '../ecoz2/src/vq/vq.c',
+    '../ecoz2/src/vq/distortion.c',
+    '../ecoz2/src/vq/report.c',
+    '../ecoz2/src/vq/sigma.c',
+    '../ecoz2/src/vq/inertia.c',
+    '../ecoz2/src/vq/quantize.c',
+    '../ecoz2/src/hmm/hmm.c',
+    '../ecoz2/src/hmm/hmm_learn.c',
+    '../ecoz2/src/hmm/hmm_classify.c',
+    '../ecoz2/src/hmm/hmm_show.c',
+    '../ecoz2/src/hmm/seq_show_files.c',
+    '../ecoz2/src/hmm/hmm_adjustb.c',
+    '../ecoz2/src/hmm/hmm_file.c',
+    '../ecoz2/src/hmm/hmm_refinement.c',
+    '../ecoz2/src/hmm/hmm_log_prob.c',
+    '../ecoz2/src/hmm/hmm_genQopt.c',
+    '../ecoz2/src/hmm/hmm_estimateB.c',
+    '../ecoz2/src/hmm/hmm_gen.c',
+    '../ecoz2/src/hmm/distr.c',
+    '../ecoz2/src/hmm/symbol.c',
+  ],
+
+  libraries=['m']
+)
 
 if __name__ == "__main__":
     ffibuilder.compile(verbose=True)
